9ffd936d67b4e98ae38020f3279f2a6edf2a73cd
George Meakin <20232931@shrewsbury.ac.uk>
2022-06-15 11:26:52 +0100

    java script starting to work

--- Script.js
+++ Script.js
@@ -1,5 +1,25 @@
+
+
 function submit(question){
-    
+
+    alert(question);
+    var qtextbox;
+    /*alert("submit run");*/
+
+    switch(question){
+        case (1):
+            qtextbox = q1TextBox;
+        case (2):
+            qtextbox = q2TextBox;
+        case (3):
+            qtextbox = q3TextBox;
+        case (4):
+            qtextbox = q4TextBox;
+    }
+
+    var qtextboxValue = document.getElementById(qtextbox).setAttribute("value", "wrong");
+    qtextboxValue = "Hello World!";
+
 }
 
 
